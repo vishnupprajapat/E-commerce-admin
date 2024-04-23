5811e13b91a95fb50f766eefecbdf6ecd4cf83c3
vishnupprajapat <prajapatvishnu767@gmail.com>
2024-04-23 11:56:10 +0530

    add api list in billboard

--- src/app/(dashboard)/[storeId]/(routes)/billboards/components/BillboardClient.tsx
+++ src/app/(dashboard)/[storeId]/(routes)/billboards/components/BillboardClient.tsx
@@ -7,6 +7,7 @@ import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import ApiList from "@/components/ui/api-list";
 interface BillboardClientProps {
   data: BillboardColumn[];
 }
@@ -30,6 +31,9 @@ const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       </div>
       <Separator />
       <DataTable searchKey="label" columns={columns} data={data}></DataTable>
+      <Heading title="Api List" description="Api call for Billboard" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
